9f9e49d3c40f236a7f8a77b6c1e734ed38e8d21f
aaditya shirodkar <aadis@seas.upenn.edu>
2012-12-09 17:28:12 -0500

    Any print fix

--- Project/Main.hs
+++ Project/Main.hs
@@ -28,7 +28,7 @@ instance PP Reg where
   pp (Alt (Sym a) (Sym b)) = (pp (Sym a)) <> PP.char '|' <> (pp (Sym b))
   pp (Alt (Sym a) Any)     = (pp (Sym a)) <> PP.char '|' <> (pp Any)
   pp (Alt Any (Sym a))     = (pp Any) <> PP.char '|' <> (pp (Sym a))
-  pp (Alt Any a)           = (pp Any) <> PP.char '|' <> (pp a)
+  pp (Alt Any a)           = (pp Any) <> PP.char '|' <> PP.parens (pp a)
   pp (Alt a Any)           = PP.parens (pp a) <> PP.char '|' <> (pp Any)
   pp (Alt (Sym a) b)       = (pp (Sym a)) <> PP.char '|' <> PP.parens (pp b)
   pp (Alt a (Sym b))       = PP.parens (pp a) <> PP.char '|' <> (pp (Sym b))
